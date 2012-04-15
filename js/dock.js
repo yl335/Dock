d3ef4a98c300edda7ee6bbfc211cbdb9095a9411
Sara Lin <slin@splunk.com>
2012-04-14 20:26:21 -0700

    Added a sample readme file; Added comments to dock.js

--- js/dock.js
+++ js/dock.js
@@ -1,14 +1,42 @@
+/* =========================================================
+ * dock.js
+ * https://github.com/yl335/Dock/blob/master/js/dock.js
+ * =========================================================
+ *
+ * Author: @yl335, extended from Marc Robichaud's plugin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================================================= */
+
 !function( $ ) {
+	
+/* PRIVATE VARIABLES
+* ===================== */
 	var dockingElements = [],
 		windowHeight = 0,
 		totalDockedHeight = 0,
 		observers = [];
 		
+/* PUBLIC METHODS
+* ===================== */
 	var methods = {
+		// contructor
 		init: function(options) {
 			return this.each(function(index, element) {
 				var $el = $(element);
+				// wrap the docking element for height placeholder
 				$el.wrap('<div class="dock-wrapper" />').parent().css('height', $el.outerHeight());
+				// add elements from options to dockingElements array
 				dockingElements.push({
 					'$el': 			$el, 
 					'height': 		$el.outerHeight(false), 
@@ -20,6 +48,7 @@
 				updatePositions();
 			});
 		},
+		// add observer to jQuery elements when they overlaps with the docked elements
 		addObserver: function(observer) {
 			var obs = $.extend( {
 				'selector'		: [],
@@ -30,6 +59,7 @@
 			if ($el) {
 				return $el.each(function(index, element) {
 					$element = $(element);
+					// add elements from options to observers array
 					observers.push({
 						'$el'			: $element,
 						'height'		: $element.outerHeight(false),
@@ -42,14 +72,18 @@
 			}
 			return 0;
 		},
+		// return the current total docked height
 		getTotalDockedHeight: function() {
 			return totalDockedHeight;
 		}
 	};
 	
+/* PRIVATE METHODS
+* ===================== */
 	function updatePositions() {
 		var scrollTop = $(window).scrollTop();
 		$(dockingElements).each(function(index, element) {
+			// set the docking element to fixed when it's about to be scrolled above the docked elements
 			if (!element.docked && (element.offsetY - scrollTop - totalDockedHeight) < 0) {
 				element.$el.css({
 					'position'		: 'fixed',
@@ -65,6 +99,7 @@
 				element.docked = true;
 				totalDockedHeight += element.height;
 			}
+			// change the docking element to position relative when it's to be scrolled below the docked elements
 			else if (element.docked && (element.offsetY - scrollTop - element.dockedHeight >= 0)) {
 				element.$el.css({
 					'position'		: 'relative',
@@ -86,11 +121,13 @@
 		$(observers).each(function(index, observer) {
 			var aboveDocked = observer.offsetY - scrollTop - totalDockedHeight <= 0;
 			var belowDocked = observer.offsetY + observer.height - scrollTop - totalDockedHeight > 0;
+			// if the observed element is right below with the last docked element, invoke the setup function
 			if (!observer.active && aboveDocked && belowDocked) {
 				observer.$el.addClass("active");
 				observer.active = true;
 				if (observer.setup) {observer.setup(observer.$el); }
 			}
+			// if the observed element is going over the last docked element, invoke the destroy function
 			else if (observer.active && (!aboveDocked | !belowDocked)) {
 				observer.$el.removeClass("active");
 				observer.active = false;
@@ -100,6 +137,8 @@
 	}
 	observeElements();
 	
+/* PLUGIN DEFINITION
+* ===================== */
 	$.fn.dock = function(method) {
 		if (methods[method]) {
 			return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
@@ -110,6 +149,7 @@
 		}
 	}
 	
+	// observe both scrolling and resizing, update the private methods
 	$(document).scroll(updatePositions).scroll(observeElements);
 	$(window).resize(updatePositions).resize(observeElements);
 }(jQuery);
